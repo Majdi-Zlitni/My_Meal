7f88272b4cf17352872f36795d3d2c9173d64495
Majdi-Zlitni <majdiuchiha@gmail.com>
2021-04-20 21:30:24 +0100

    component

---
+++ Coding/Simple html/js/jQ.js
@@ -0,0 +1,8 @@
+$('#iconified').on('keyup', function() {
+    var input = $(this);
+    if(input.val().length === 0) {
+        input.addClass('empty');
+    } else {
+        input.removeClass('empty');
+    }
+});
