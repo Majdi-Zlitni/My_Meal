1def431f971335f7fa0cc5bafa781f966addc86a
Majdi-Zlitni <majdiuchiha@gmail.com>
2021-05-03 06:00:11 +0100

    VBE

--- VBE/backend/main.py
+++ VBE/backend/main.py
@@ -2,13 +2,12 @@ from typing import Optional
 from fastapi import FastAPI , Request
 import mysql.connector
 import json
+from starlette.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()#snaana Instance mel fastapi mteena
 
-origins = [
-    "http://localhost",
-    "http://localhost:4200",
-]
+origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -53,16 +52,21 @@ def gets():
     for result in rv:
         json_data.append(dict(zip(row_headers,result)))
     return json_data
+
 #Send data of reclamation
 @app.post("/reclam")
 async def reclam(request:Request):
-    mydb = mysql.connector.connect(host = "localhost" , user = "root" , password = "" , database = "mymeal")
-    mycursor = mydb.cursor()
+    mydb = mysql.connector.connect(host = "localhost" , user = "root" , password = "" , database = "mymeal")#Connectiw ala el BD mteena
+    mycursor = mydb.cursor()#snaana cursor ala el db 
     body = json.loads(await request.body())
+    #khdhina el body mtaa el requete post mteena w radineh objet json
+    print(body)
+    #executina instruction mtaa insert w ki hachtna b variable mel body il suffit naamlou {body['{esm_el_attribut}']}
     try:
-        mycursor.execute(f"INSERT INTO `{body['type']}` ( `email`, `reclam`) VALUES ( '{body['email']}', '{body['reclam']}');")
-        mydb.commit()
+        mycursor.execute(f"INSERT INTO reclamation ( email, reclam,dataereclam) VALUES ( '{body['email']}', '{body['reclam']}');")
+        mydb.commit()#commit bech nsajlou fel BD
         return {"ok"}
     except:
-        mydb.rollback()
-        return ("no")
+        mydb.rollback
+        return {"non"}
+        app.get("/meals")#hedhi GET
--- angular/src/app/contactinfo/contactinfo.component.ts
+++ angular/src/app/contactinfo/contactinfo.component.ts
@@ -10,21 +10,24 @@ export class ContactinfoComponent implements OnInit {
   constructor() { }
   ngOnInit(): void {
   }
-  async reclam(){
+  async reclam() {
     var email = (<HTMLInputElement>document.getElementById("email")).value
     var reclam = (<HTMLInputElement>document.getElementById("reclam")).value
-    var body = {"email":"${email}" , "reclam":"${reclam}"} 
+    var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/')
+    var body = `{ "email": "${email}", "	message": "${reclam}"}`
 
-const rep = await fetch("http://127.0.0.1:8000/reclam" , {
-  method:"POST" , 
-  body: "body"
-})
-if (rep.ok){
-  rep.json().then((data)=>{ 
-  })
-}
+    console.log(email,reclam,utc,body)
+    const rep = await fetch("http://127.0.0.1:8000/reclam", {
+      method: "POST",
+      body: body
+    })
+    console.log(body)
+    if (rep.ok) {
+      rep.json().then((data) => {
+      })
+    }
 
-}
+  }
 
 
 }
